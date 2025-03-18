9d1903931d2087e1138bbc21e3bdab5a2b467305
hellomyzn <em711th@icloud.com>
2025-03-18 22:41:42 +0900

    update: main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
+from common.log import initialize_logger
 from controllers import HealthController
 
-if __name__ == "__main__":
+
+def main():
+    initialize_logger()
+
     input_file = "src/apple_health_export/export.xml"
     USE_GOOGLE_SHEETS = True
     SPREADSHEET_ID = "your_google_spreadsheet_id"
 
     controller = HealthController(use_google_sheets=USE_GOOGLE_SHEETS, spreadsheet_id=SPREADSHEET_ID)
     controller.process_health_data(input_file)
+
+
+if __name__ == "__main__":
+    main()
